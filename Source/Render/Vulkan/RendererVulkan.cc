4f30e5150b57d6221bc5b8805da1b3358f735ada
Floyden <keisen95@gmail.comx>
2023-03-10 18:56:40 +0100

    Fix crash on macOs

--- Source/Render/Vulkan/RendererVulkan.cc
+++ Source/Render/Vulkan/RendererVulkan.cc
@@ -1,5 +1,6 @@
 #include "RendererVulkan.hh"
 
+#include <iostream>
 namespace Solis::Render
 {
     
@@ -10,11 +11,27 @@ RendererVulkan::RendererVulkan()
 RendererVulkan::~RendererVulkan()
 {
 }
-
 void RendererVulkan::Initialize() 
 {
+    std::vector<const char*> enabledExtensions;
+    std::vector<vk::ExtensionProperties> extensionProperties = vk::enumerateInstanceExtensionProperties();
+    auto it = std::find_if(
+        extensionProperties.begin(), 
+        extensionProperties.end(), 
+        [](auto& a) { return std::strcmp(a.extensionName, VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME) == 0;}
+    );
+    
+    if (it != extensionProperties.end()) {
+        enabledExtensions.push_back(it->extensionName);
+    }
+
     vk::ApplicationInfo applicationInfo( "AppName", 1, "EngineName", 1, VK_API_VERSION_1_1 );
-    vk::InstanceCreateInfo instanceCreateInfo( {}, &applicationInfo );
+    vk::InstanceCreateInfo instanceCreateInfo( 
+        vk::InstanceCreateFlagBits::eEnumeratePortabilityKHR, 
+        &applicationInfo,
+        0, nullptr,
+        enabledExtensions.size(), enabledExtensions.data()
+    );
     mInstance = vk::createInstance( instanceCreateInfo );
 }
 
