50d66837751e0e5a8415ea04bbec77af6c2e02bd
Floyden <floyden@fnaa.net>
2023-06-21 20:45:15 +0200

    Add Cube Shape

--- Source/Render/Shapes.hh
+++ Source/Render/Shapes.hh
@@ -72,4 +72,86 @@ private:
     float mSize;
 };
 
+class Cube : public Shape
+{
+public:
+    Cube(float size) : mSize(size) {}
+    Vector<float> GetPositions() const override
+    {
+        auto halfSize = mSize / 2.0f;
+        Vector<float> cube = 
+        {
+            -halfSize, -halfSize, -halfSize,
+             halfSize, -halfSize, -halfSize,
+            -halfSize,  halfSize, -halfSize,
+             halfSize,  halfSize, -halfSize,
+            -halfSize, -halfSize, halfSize,
+             halfSize, -halfSize, halfSize,
+            -halfSize,  halfSize, halfSize,
+             halfSize,  halfSize, halfSize,
+        };
+
+        return cube;
+    }
+
+    Vector<float> GetUVs() const override
+    {
+        Vector<float> cube = 
+        {
+            0.0, 0.0,
+            1.0, 0.0,
+            0.0, 1.0,
+            1.0, 1.0,
+            0.0, 0.0,
+            1.0, 0.0,
+            0.0, 1.0,
+            1.0, 1.0,
+        };
+
+        return cube;
+    }
+
+    Vector<float> GetNormals() const override
+    {
+        Vector<float> cube = 
+        {
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+        };
+
+        return cube;
+    }
+
+    Vector<uint32_t> GetIndices() const override
+    {
+        Vector<uint32_t> indices = 
+        {
+            0, 1, 3,
+            3, 2, 0,
+
+            4, 5, 7,
+            7, 6, 4,
+
+            6, 2, 0,
+            0, 4, 6,
+
+            7, 3, 1,
+            1, 5, 7,
+
+            0, 1, 5,
+            5, 4, 0,
+
+            2, 3, 7,
+            7, 6, 2,
+        };
+
+        return indices;
+    }
+private:
+    float mSize;
+};
+
+
 } // namespace Solis
