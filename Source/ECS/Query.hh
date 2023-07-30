1fcb9c9689dcd37a74ddc7c7e470038429f49128
Floyden <keisen95@gmail.comx>
2023-07-30 20:41:37 +0200

    Create a QueryIter

--- Source/ECS/Query.hh
+++ Source/ECS/Query.hh
@@ -1,4 +1,5 @@
 #pragma once
+#include "Entity.hh"
 
 namespace Solis::ECS
 {
@@ -7,21 +8,60 @@ class World;
 struct QueryBase {};
 
 template<typename... Args>
-struct Query : public QueryBase
+class Query : public QueryBase
 {
 public:
-
     using Types = typename std::tuple<Args...>;
     static constexpr size_t TypeCount = sizeof...(Args);
     
     template<int N>
     using TypeOf = typename std::tuple_element<N, Types>::type;
 
-    Query(World& world) : mWorld(world) {};
+    struct QueryIter
+    {
+        bool operator==(QueryIter const & other) const { return mCurrent == other.mCurrent; }
+        bool operator!=(QueryIter const & other) const { return !(*this == other); }
+
+        QueryIter operator++() 
+        {
+            mCurrent = FindNext();
+            return *this; 
+        }
+
+        QueryIter operator++(int) 
+        {
+            auto old = *this;
+            mCurrent = FindNext();
+            return old; 
+        }
+
+        Query::Types operator*() { 
+            return CreateReference(); }
+
+    private:
+        QueryIter(Query const& query) : mCurrent(std::nullopt), mQuery(query) {};
+
+        Optional<Entity> FindNext() const;
+        Query::Types CreateReference() const;
+
+        Optional<Entity> mCurrent;
+        Query<Args...> const& mQuery;
+
+        friend class Query<Args...>;
+    };
+
+public:
+    Query(World& world) : mWorld(world) 
+    {
+        static_assert((std::is_reference_v<Args> && ...));
+    };
 
     std::tuple<Args...> GetSingle();
 
     void Test();
+
+    QueryIter begin() const { return ++QueryIter(*this); }
+    QueryIter end() const { return QueryIter(*this); }
 
 public:
     // Container functions
