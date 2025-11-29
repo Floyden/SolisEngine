2fab223bf7abaa6a87e04d3a002ccfd6bd9583e5
Floyden <floyden@fnaa.net>
2025-11-29 12:53:59 +0100

    More consistent event handling

--- src/core/system_event.zig
+++ src/core/system_event.zig
@@ -12,30 +12,30 @@ pub const SystemEvent = union {
 };
 
 pub fn handleSystemEvents(allocator: std.mem.Allocator, world: *World) !void {
-    var system_events = try EventWriter(SystemEvent).init(world, 0);
-    var window_events = try EventWriter(Window.Event).init(world, 0);
-    var input_events = try EventWriter(KeyEvent).init(world, 0);
     var event: c.SDL_Event = undefined;
     while (c.SDL_PollEvent(&event)) {
         switch (event.type) {
-            c.SDL_EVENT_QUIT, c.SDL_EVENT_WINDOW_CLOSE_REQUESTED => system_events.emit(allocator, .{ .close_request = true }) catch @panic("OOM?"),
-            c.SDL_EVENT_WINDOW_RESIZED => try window_events.emit(allocator, .{ .resized = Window.Resized{
-                .window = event.window.windowID,
-                .width = @intCast(event.window.data1),
-                .height = @intCast(event.window.data2),
-            } }),
-            c.SDL_EVENT_KEY_DOWN => input_events.emit(allocator, .{
-                .down = true,
-                .key_code = event.key.key,
-                .scan_code = event.key.scancode,
-                .mod = event.key.mod,
-            }) catch @panic("OOM?"),
-            c.SDL_EVENT_KEY_UP => input_events.emit(allocator, .{
-                .down = false,
-                .key_code = event.key.key,
-                .scan_code = event.key.scancode,
-                .mod = event.key.mod,
-            }) catch @panic("OOM?"),
+            c.SDL_EVENT_QUIT, c.SDL_EVENT_WINDOW_CLOSE_REQUESTED => {
+                var system_events = try EventWriter(SystemEvent).init(world, 0);
+                try system_events.emit(allocator, .{ .close_request = true });
+            },
+            c.SDL_EVENT_WINDOW_RESIZED => {
+                var window_events = try EventWriter(Window.Event).init(world, 0);
+                try window_events.emit(allocator, .{ .resized = Window.Resized{
+                    .window = event.window.windowID,
+                    .width = @intCast(event.window.data1),
+                    .height = @intCast(event.window.data2),
+                } });
+            },
+            c.SDL_EVENT_KEY_DOWN, c.SDL_EVENT_KEY_UP => {
+                var input_events = try EventWriter(KeyEvent).init(world, 0);
+                try input_events.emit(allocator, .{
+                    .down = event.key.down,
+                    .key_code = event.key.key,
+                    .scan_code = event.key.scancode,
+                    .mod = event.key.mod,
+                });
+            },
             c.SDL_EVENT_MOUSE_MOTION => {
                 var writer = try EventWriter(input.MouseMotionEvent).init(world, 0);
                 const motion = event.motion;
